70a604b0e617c999499c609d09f18866575a8fc0
Gabis <zikmanis1@gmail.com>
2021-10-26 18:58:43 +0300

    Saving big changes what is made in this project! What i remmber i just now updated code in updater and remade setup because there was problem what i didnt saw but now thei are fixed....

--- SuperTuxTrainer/LogIn.cs
+++ SuperTuxTrainer/LogIn.cs
@@ -44,7 +44,7 @@ namespace Login_HWID
             WebClient webClient = new WebClient();
             try
             {
-                if (!webClient.DownloadString("https://gabhx.000webhostapp.com/Updating/version.txt").Contains("11"))
+                if (!webClient.DownloadString("https://gabhx.000webhostapp.com/Updating/version.txt").Contains("10"))
                 {
 
 
