5a4f911a4cb10c557b42c0686872ff79054fc220
Gabis <45454549+Programmetajs@users.noreply.github.com>
2022-12-04 12:16:34 +0200

    Deleted some folders

--- SuperTuxTrainer/Program.cs
+++ SuperTuxTrainer/Program.cs
@@ -18,8 +18,8 @@ namespace SuperTuxTrainer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
-            Application.Run(new MainForm());
+            Application.Run(new Login());
+            //Application.Run(new MainForm());
 
         }
     }
